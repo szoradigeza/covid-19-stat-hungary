cc674cef6c5d4ef1f71423deba68e018036544da
szoradigeza <szoradigeza@gmail.com>
2020-04-02 13:08:43 +0200

    Updates

---
+++ precache-manifest.4bb8654375260203c1c7afc7393a0ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd637fa194307982a07876ce4c55ed6",
+    "url": "/covid-19-stat-hungary/index.html"
+  },
+  {
+    "revision": "c1e419c3c1f612fb27be",
+    "url": "/covid-19-stat-hungary/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b7a52bd9745335c2589d",
+    "url": "/covid-19-stat-hungary/static/js/2.a98c8b20.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/covid-19-stat-hungary/static/js/2.a98c8b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e419c3c1f612fb27be",
+    "url": "/covid-19-stat-hungary/static/js/main.5f765545.chunk.js"
+  },
+  {
+    "revision": "30ed35fd820055d6cf0b",
+    "url": "/covid-19-stat-hungary/static/js/runtime-main.759f4af5.js"
+  }
+]);
